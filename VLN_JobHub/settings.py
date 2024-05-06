d25371301d981e48b371de2481f4ea2282e6e36a
Hlynur Isak <isakvillason@hotmail.com>
2024-05-06 14:48:00 +0000

    update db string

--- VLN_JobHub/settings.py
+++ VLN_JobHub/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = "django-insecure-mo0_+(944l7pp(c3$sb+o#-+dgpmun%b#!o%&jkpj)w(_u72+8
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,17 +69,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "VLN_JobHub.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
+DATABASES = {'default':
+                 {'ENGINE': 'django.db.backends.postgresql',
+                  'NAME': 'postgres',
+                  'USER': 'vln2_assignment_groups_<group_number>_user',
+                  'PASSWORD': 'yourpassword',
+                  'HOST': 'verklegt-namskeid-ii.northeurope.cloudapp.azure.com',
+                  'PORT': '5432',
+                  'OPTIONS': {
+                      'options': '-c search_path=vln2_assignment_groups_<group_number>'}
+                  }
+             }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -101,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -112,7 +112,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
