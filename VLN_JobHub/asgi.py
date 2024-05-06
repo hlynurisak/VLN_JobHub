8d4b26459bd3f437350e542a51df50367ec6b60e
Hlynur Isak <isakvillason@hotmail.com>
2024-05-06 11:26:07 +0000

    update

---
+++ VLN_JobHub/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for VLN_JobHub project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "VLN_JobHub.settings")
+
+application = get_asgi_application()
