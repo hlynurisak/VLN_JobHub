707db7b2b012afcf7e462b98b7d919d7877d2c0c
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2024-05-07 19:11:40 +0000

    Testing adding new apps to the project, maybe not required

--- VLN_JobHub/urls.py
+++ VLN_JobHub/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("home/", include("JobHub.urls")),
+    path("companies/", include("company.urls")),
+
 ]
--- company/views.py
+++ company/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+
+# Create your views here.
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello from index view in home app")
+    return HttpResponse("<h1>Hello from index view in company app</h1>")
