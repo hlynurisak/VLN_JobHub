784bad5ae5010db6fd266755d9586019a7b4296d
robert-orri <robertorriarnason@gmail.com>
2024-05-09 16:52:04 +0000

    Populata síðuna

--- JobHub/views.py
+++ JobHub/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 companies = [
     {'name': 'Coca Cola', 'job_listing': "Data Analyst"},
-    {'name': 'Dominos', 'job_listing': "Computer Scientist"}
+    {'name': 'Dominos', 'job_listing': "Computer Scientist"},
+    {'name': 'CCP', 'job_listing': "Nerd"},
+    {'name': 'Elko', 'job_listing': "Trúður í Verslun"},
+    {'name': 'Sýn', 'job_listing': "Fréttamaður"},
+    {'name': 'Rúv', 'job_listing': "Gott framlag í Eurovision"},
+
 ]
 # Create your views here.
 def index(request):
