b08b5095217012237d96dca721a575a14cc2e65a
Hlynur Isak <isakvillason@hotmail.com>
2024-05-10 17:09:27 +0000

    User Profile grunnur kominn

--- user/views.py
+++ user/views.py
@@ -1,7 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
+
 # Create your views here.
+companies = [
+    {'name': 'Coca Cola', 'job_listing': "Data Analyst"},
+    {'name': 'Dominos', 'job_listing': "Computer Scientist"},
+    {'name': 'CCP', 'job_listing': "Nerd"},
+    {'name': 'Elko', 'job_listing': "Trúður í Verslun"},
+    {'name': 'Sýn', 'job_listing': "Fréttamaður"},
+    {'name': 'Rúv', 'job_listing': "Gott framlag í Eurovision"},
+
+]
+
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(data=request.POST)
@@ -12,3 +24,7 @@ def register(request):
     return render(request, 'user/register.html', {
         'form': UserCreationForm()
     })
+
+
+def profile(request):
+    return render(request, 'user/profile.html', context={'companies': companies})
