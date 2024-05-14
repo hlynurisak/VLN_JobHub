38fc5f84f861fd1cfbee8340288b801eb447f11b
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2024-05-14 16:22:09 +0000

    Bunch of UI updates especially for the main page

--- company/views.py
+++ company/views.py
@@ -3,7 +3,5 @@ from company.models import Company
 # Create your views here.
 def index(request):
     return render(request, 'company/index.html',{
-        'comapany': Company.objects.all()
+        'company': Company.objects.all()
     })
-
-"Testing"
