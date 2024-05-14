c1146240df16bb10dc318d74e5de8a8ad26f2340
Hlynur Isak <isakvillason@hotmail.com>
2024-05-14 11:36:38 +0000

    rm old comments and follow pep8

--- VLN_JobHub/settings.py
+++ VLN_JobHub/settings.py
@@ -76,10 +76,6 @@ WSGI_APPLICATION = "VLN_JobHub.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#Ekki uncommenta þennan kóða, þá getum við ekki testað neitt, annaðhvort það eða
-#Laga tenginguna
-
-
 DATABASES = {'default':
              {'ENGINE': 'django.db.backends.postgresql',
               'NAME': 'postgres',
