9949051885f8a0fca7e99b739e8b088a0e9c0287
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2024-05-14 17:06:07 +0000

    Company screen updated

--- company/views.py
+++ company/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from company.models import Company
 # Create your views here.
 def index(request):
-    return render(request, 'company/index.html',{
+    return render(request, 'company/companies.html', {
         'company': Company.objects.all()
     })
