cfd6467cd7913ae74165aeecab1532a966216119
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2024-05-15 15:43:04 +0000

    Company Info page added cards updated

--- company/urls.py
+++ company/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='company-index'),
+    path('company-info/<int:company_id>/', views.company_info, name='company-info'),
 ]
--- company/views.py
+++ company/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from company.models import Company
 from JobHub.models import Job
 # Create your views here.
@@ -6,3 +6,7 @@ def index(request):
     return render(request, 'company/companies.html', {
         'company': Company.objects.all().order_by('name')
     })
+
+def company_info(request, company_id):
+    company = get_object_or_404(Company, id=company_id)
+    return render(request, 'company/company-info.html', {'company': company})
