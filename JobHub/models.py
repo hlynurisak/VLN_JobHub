3daa0bdc2dfd4a038ffd3e381915d96905017014
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2024-05-15 14:22:04 +0000

    A lot of UI updates

--- JobHub/models.py
+++ JobHub/models.py
@@ -13,15 +13,15 @@ class Job(models.Model):
     description = models.CharField(max_length=255, blank=True)
     location = models.CharField(max_length=255, blank=True)
     category = models.ForeignKey(JobCategory, on_delete=models.CASCADE)
-    job_requirements = models.FloatField
+    job_requirements = models.FloatField()
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class CompanyImage(models.Model):
-    image = models.CharField(max_length=9999)
-    Job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    image = models.URLField(max_length=9999)
+    company = models.ForeignKey(Company, related_name="images", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.image
--- company/models.py
+++ company/models.py
@@ -4,15 +4,9 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=255)
     CompanyStart_date = models.DateTimeField()
-    logo = models.CharField(max_length=9999, blank=True)
 
     def __str__(self):
         return self.name
 
-
-class CompanyImages(models.Model):
-    image = models.CharField(max_length=9999, null=True)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.image
+    def get_first_image(self):
+        return self.images.first().image
