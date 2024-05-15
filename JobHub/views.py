73a0548d54f0f928568cbb298583996fb56b26f0
robert-orri <robertorriarnason@gmail.com>
2024-05-15 12:11:28 +0000

    Búa til get og post requests

--- JobHub/views.py
+++ JobHub/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+import JobHub
 from JobHub.models import Job
 
 
@@ -6,3 +8,18 @@ from JobHub.models import Job
 def index(request):
     context = {'JobHub': Job.objects.all().order_by('name')}
     return render(request, 'JobHub/home_page.html', context)
+
+def get_job_by_id(request, id):
+    return render('JobHub/home_page.html',{
+        'Jobjub': get_object_or_404(JobHub, pk=id)
+    })
+
+
+#def create_job(request):
+#    if request.method == 'POST':
+#        print(1)
+#   else:
+#        print(2)
+#    return render(request, 'JobHub/create_job.html', {
+#        'form': form
+#    })
