1aced84d0962e2ec51faf32a3315abd1735c151e
robert-orri <robertorriarnason@gmail.com>
2024-05-16 09:54:48 +0000

    smá vðbæting

--- JobHub/forms/jobhub_form.py
+++ JobHub/forms/jobhub_form.py
@@ -11,7 +11,7 @@ class JobCreateForm(ModelForm):
             'name': widgets.TextInput(attrs={'class': 'form-control'}),
             'description': widgets.TextInput(attrs={'class': 'form-control'}),
             'category': widgets.Select(attrs={'class': 'form-control'}),
-            'due_date': widgets.DateInput(attrs={'class': 'form-control'}),
+            'due_date': widgets.TextInput(attrs={'class': 'form-control'}),
             'company': widgets.Select(attrs={'class': 'form-control'}),
-            'start_date': widgets.DateInput(attrs={'class': 'form-control'}),
+            'start_date': widgets.TextInput(attrs={'class': 'form-control'}),
         }
