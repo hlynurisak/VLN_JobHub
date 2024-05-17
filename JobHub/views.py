d266c0cb34e530204401bdf7a9e7b27a651ff503
robert-orri <robertorriarnason@gmail.com>
2024-05-17 18:20:43 +0000

    Filter BABY

--- JobHub/views.py
+++ JobHub/views.py
@@ -1,30 +1,60 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
-import JobHub
+from django.db.models import Q
 from JobHub.forms.jobhub_form import JobCreateForm
-from JobHub.models import Job, CompanyImage
+from JobHub.models import Job, JobCategory, Company
 
-
-# Create your views here.
 def index(request):
-    context = {'JobHub': Job.objects.all().order_by('name')}
+    job_offers = Job.objects.all()
+    categories = JobCategory.objects.all()
+    companies = Company.objects.all()
+
+    # Filtering
+    category_id = request.GET.get('category')
+    if category_id:
+        job_offers = job_offers.filter(category_id=category_id)
+
+    company_id = request.GET.get('company')
+    if company_id:
+        job_offers = job_offers.filter(company_id=company_id)
+
+    applied_filter = request.GET.get('applied')
+    if applied_filter:
+        # Assuming 'applied' is a boolean field in the Job model
+        job_offers = job_offers.filter(applied=applied_filter)
+
+    # Searching
+    search_query = request.GET.get('search')
+    if search_query:
+        job_offers = job_offers.filter(Q(name__icontains=search_query) | Q(description__icontains=search_query))
+
+    # Ordering
+    order_by = request.GET.get('order_by')
+    if order_by == 'date':
+        job_offers = job_offers.order_by('-start_date')
+    elif order_by == 'due_date':
+        job_offers = job_offers.order_by('due_date')
+
+    context = {
+        'job_offers': job_offers,
+        'categories': categories,
+        'companies': companies
+    }
     return render(request, 'JobHub/home_page.html', context)
 
 def get_job_by_id(request, id):
-    return render('JobHub/home_page.html',{
-        'JobHub': get_object_or_404(JobHub, pk=id)
+    return render(request, 'JobHub/home_page.html', {
+        'JobHub': get_object_or_404(Job, pk=id)
     })
-
 
 def create_joblisting(request):
     if request.method == 'POST':
-        form =JobCreateForm(data=request.POST)
+        form = JobCreateForm(data=request.POST)
         if form.is_valid():
             job = form.save()
             job.save()
             return redirect('/')
     else:
-        form = JobCreateForm
+        form = JobCreateForm()
     return render(request, 'JobHub/create_joblisting.html', {
         'form': form
     })
