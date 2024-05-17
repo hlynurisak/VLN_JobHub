19b264f388691e8e3c368c441ca1b193008b09cd
RunicSyntax <107773663+RunicSyntax@users.noreply.github.com>
2024-05-17 20:59:21 +0000

    Application model import hotfix

--- application/models.py
+++ application/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from JobHub.models import Job, Company, User
+from JobHub.models import Job, Company
+from django.contrib.auth.models import User
 
 
 class ContactInformation(models.Model):
