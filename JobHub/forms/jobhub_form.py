996b467e1892e7762f6e452e0989f71427cedf7f
robert-orri <robertorriarnason@gmail.com>
2024-05-17 14:55:01 +0000

    Minor charges(allegedly)

--- JobHub/forms/jobhub_form.py
+++ JobHub/forms/jobhub_form.py
@@ -1,5 +1,5 @@
-from django.forms import ModelForm, widgets
 from django import forms
+from django.forms import ModelForm, widgets
 from JobHub.models import Job
 
 class JobCreateForm(ModelForm):
@@ -10,8 +10,8 @@ class JobCreateForm(ModelForm):
             'name': widgets.TextInput(attrs={'class': 'form-control'}),
             'description': widgets.TextInput(attrs={'class': 'form-control'}),
             'category': widgets.Select(attrs={'class': 'form-control'}),
-            'due_date': widgets.DateInput(attrs={'class': 'form-control'}),
+            'due_date': widgets.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'company': widgets.Select(attrs={'class': 'form-control'}),
-            'start_date': widgets.DateInput(attrs={'class': 'form-control'}),
-            'job_type': forms.Select(attrs={'class': 'form-control'}),
+            'start_date': widgets.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'job_type': widgets.Select(attrs={'class': 'form-control'}),
         }
